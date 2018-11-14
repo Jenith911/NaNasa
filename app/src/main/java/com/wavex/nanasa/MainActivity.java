c63c22f159e460fc21e83b9e5e6bcf142b813c8a
jenster123 <41296924+jenster123@users.noreply.github.com>
2018-11-14 12:42:19 +0530

    Login Page Done

--- app/src/main/java/com/wavex/nanasa/MainActivity.java
+++ app/src/main/java/com/wavex/nanasa/MainActivity.java
@@ -1,11 +1,16 @@
 package com.wavex.nanasa;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.support.annotation.NonNull;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import com.facebook.AccessToken;
@@ -24,14 +29,24 @@ import com.google.firebase.auth.FacebookAuthCredential;
 import com.google.firebase.auth.FacebookAuthProvider;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.wavex.nanasa.Model.User;
 
 import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
-
+    EditText edtUser,edtPassword;
+    EditText edtNewUser,edtNewPassword,edtNewEmail;
+    Button btnSignUp,btnSignIn;
     LoginButton loginButton;
     CallbackManager callbackManager;
     private static final String EMAIL = "email";
+    FirebaseDatabase database;
+    DatabaseReference users;
     private FirebaseAuth mAuth;
 
     @Override
@@ -40,13 +55,97 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         FacebookSdk.sdkInitialize(getApplicationContext());
         AppEventsLogger.activateApp(this);
+        //Firebase
         mAuth = FirebaseAuth.getInstance();
+        database=FirebaseDatabase.getInstance();
+        users = database.getReference("Users");
 
+        edtUser = findViewById(R.id.editUser);
+        edtPassword = findViewById(R.id.editPassword);
+        btnSignIn = (Button)findViewById(R.id.btn_sign_in);
+        btnSignUp = (Button)findViewById(R.id.btn_sign_up);
 
+        btnSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showSignUpDialog();
+            }
+        });
+        btnSignIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                signIn(edtUser.getText().toString(),edtPassword.getText().toString());
+            }
+        });
         loginButton = (LoginButton) findViewById(R.id.login_button);
         callbackManager = CallbackManager.Factory.create();
         loginButton.setReadPermissions(Arrays.asList(EMAIL));
 
+    }
+
+    private void signIn(final String user,final String pwd) {
+        users.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                if(dataSnapshot.child(user).exists()){
+                    if(!user.isEmpty()){
+                        User login = dataSnapshot.child(user).getValue(User.class);
+                        if(login.getPassword().equals(pwd)){
+                            Toast.makeText(MainActivity.this,"Login ok !",Toast.LENGTH_SHORT).show();
+                        }else{
+                            Toast.makeText(MainActivity.this,"Wrong password",Toast.LENGTH_SHORT).show();
+                        }
+                    }else{
+                        Toast.makeText(MainActivity.this,"Please enter your User Name",Toast.LENGTH_SHORT).show();
+                    }
+                }else{
+                    Toast.makeText(MainActivity.this,"User does not exist",Toast.LENGTH_SHORT).show();
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+    }
+
+    private void showSignUpDialog() {
+        AlertDialog.Builder alertDialog = new AlertDialog.Builder(MainActivity.this);
+        alertDialog.setTitle("Sign Up");
+        alertDialog.setMessage("Please fill full Information");
+
+        LayoutInflater inflater = this.getLayoutInflater();
+        View sign_up_layout = inflater.inflate(R.layout.login_layout,null);
+        edtNewUser = sign_up_layout.findViewById(R.id.editNewUserName);
+        edtNewPassword = sign_up_layout.findViewById(R.id.editNewPassword);
+        edtNewEmail = sign_up_layout.findViewById(R.id.editNewEmail);
+
+        alertDialog.setView(sign_up_layout);
+        alertDialog.setIcon(R.drawable.ic_account_circle_black_24dp);
+        alertDialog.setNegativeButton("Yes", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                final User user = new User(edtNewUser.getText().toString(),
+                        edtNewPassword.getText().toString(),edtNewEmail.getText().toString());
+            users.addListenerForSingleValueEvent(new ValueEventListener() {
+                @Override
+                public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                    if(dataSnapshot.child(user.getUserName()).exists())
+                        Toast.makeText(MainActivity.this,"User already exists",Toast.LENGTH_SHORT).show();
+                    else{
+                        users.child(user.getUserName()).setValue(user);
+                        Toast.makeText(MainActivity.this,"User regitration success",Toast.LENGTH_SHORT).show();
+                    }
+                }
+
+                @Override
+                public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                }
+            });
+            }
+        });
     }
 
     public void ButtonClickLoginFb(View v){
